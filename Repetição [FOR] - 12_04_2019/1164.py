4f12de50ef23495a04e55ad23dd81af9c5d2e4b0
Thairam <thayram01@gmail.com>
2019-04-12 18:41:57 -0300

    Add files via upload

---
+++ Repetição [FOR] - 12_04_2019/1164.py
@@ -0,0 +1,14 @@
+n = int(input())
+while n > 0:
+    n -= 1
+
+    x = int(input())
+    soma = 0
+    for i in range(1, x):
+        if x%i == 0:
+            soma += i
+
+    if soma == x:
+        print('{} eh perfeito'.format(x))
+    else:
+        print('{} nao eh perfeito'.format(x))
